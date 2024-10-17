6a69ae07e4dc1288d0ad04157ff90ded0a0f02ee
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-10-17 12:09:45 +0200

    Border countries are displayed. Will try to add flag image and update the styles further

--- src/Components/CountryInfo/CountryInfo.tsx
+++ src/Components/CountryInfo/CountryInfo.tsx
@@ -5,33 +5,50 @@ import axios from 'axios';
 import { BASE_URL } from '../../constants.ts';
 
 interface Props {
-  code: string;
+  code: string | null;
 }
 
 const URL_TO_GET_ONE_COUNTRY = BASE_URL + 'alpha/';
 
 const CountryInfo: React.FC<Props> = ({code}) => {
   const [country, setCountry] = useState<APICountry | null>(null);
+  const [borderCountries, setBorderCountries] = useState<string[]>([]);
 
   useEffect(() => {
     const getCountryByCode = async () => {
       const responseReq = await axios.get<APICountry>(URL_TO_GET_ONE_COUNTRY + code);
       setCountry(responseReq.data);
+
+      if (responseReq.data.borders) {
+        const borderPromises = responseReq.data.borders.map(async(borderCode: string) => {
+          const borderResponse = await axios.get<{name: string}>(URL_TO_GET_ONE_COUNTRY + borderCode);
+          return borderResponse.data.name;
+        });
+        const borderNames = await Promise.all(borderPromises);
+        setBorderCountries(borderNames);
+      } else {
+        setBorderCountries([]);
+      }
     };
+
+
 
     if (code) {
       void getCountryByCode();
     }
   }, [code]);
 
-  return  country && (
+  return  country ? (
     <div className="mt-4">
       <h4>{country.name}</h4>
       <div>
-        <p>Country information: {country.capital}</p>
-        <p>Borders</p>
+        <p><strong>Capital: </strong>{country.capital}</p>
+        <p><strong>Population: </strong>{country.population}</p>
+        <p><strong>Borders with: </strong>{borderCountries.length > 0 ? borderCountries : 'No bordering countries'}</p>
       </div>
     </div>
+  ) : (
+    <div>Country not found</div>
   );
 };
 
