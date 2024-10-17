b20c299e94c68f126b7d41efbf4fe0fa0197d49e
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-10-17 10:02:51 +0200

    Moved the functionality part to the Country container and added argument countries as Props to the CountryList component. Works properly - moving on to the next task

--- src/Components/CountryList/CountryList.tsx
+++ src/Components/CountryList/CountryList.tsx
@@ -1,25 +1,13 @@
 import * as React from 'react';
-import { useCallback, useEffect, useState } from 'react';
 import { ICountry } from '../../types';
-import MakeRequest from '../../Helpers/MakeRequest.ts';
-import { BASE_URL, LIST_URL } from '../../constants.ts';
+
+interface Props {
+  countries: ICountry[];
+}
 
 
-const CountryList: React.FC = () => {
-  const [countries, setCountries] = useState<ICountry[]>([]);
+const CountryList: React.FC<Props> = ({countries}) => {
 
-  const fetchData = useCallback(async () => {
-    try {
-      const data = await MakeRequest<ICountry[]>(BASE_URL + LIST_URL);
-      setCountries(data);
-    } catch (error) {
-      console.log (error);
-    }
-  }, []);
-
-  useEffect(() => {
-    void fetchData();
-  }, [fetchData]);
 
   return (
     <div>
--- src/Containers/Country/Country.tsx
+++ src/Containers/Country/Country.tsx
@@ -1,11 +1,30 @@
 import CountryList from '../../Components/CountryList/CountryList.tsx';
 import CountryInfo from '../../Components/CountryInfo/CountryInfo.tsx';
+import { useCallback, useEffect, useState } from 'react';
+import { ICountry } from '../../types';
+import MakeRequest from '../../Helpers/MakeRequest.ts';
+import { BASE_URL, LIST_URL } from '../../constants.ts';
 
 
 const Country = () => {
+  const [countries, setCountries] = useState<ICountry[]>([]);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const data = await MakeRequest<ICountry[]>(BASE_URL + LIST_URL);
+      setCountries(data);
+    } catch (error) {
+      console.log (error);
+    }
+  }, []);
+
+  useEffect(() => {
+    void fetchData();
+  }, [fetchData]);
+
   return (
     <div className="container mt-4">
-      <CountryList/>
+      <CountryList countries={countries}/>
       <CountryInfo/>
     </div>
   );
